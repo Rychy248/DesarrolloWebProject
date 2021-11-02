8661d5b9142b5b5a1b9b2b6b6f9ba61a573b19fc
Rychy <jorgeajrhh@gmail.com>
2021-11-01 18:53:11 -0600

    settings and models modifyded

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -95,19 +95,19 @@ AUTH_USER_MODEL = 'accounts.Account'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
    
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
-
 DATABASES = {
-    'default':dj_database_url.config(
-        default=config('DATABASE_URL')
-        )    
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-#
+
+# DATABASES = {
+#     'default':dj_database_url.config(
+#         default=config('DATABASE_URL')
+#         )    
+# }
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
@@ -177,5 +177,5 @@ EMAIL_USE_TLS = config('EMAIL_USE_TLS',cast=bool)
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-DISABLE_COLLECTSTATIC=False
+#DISABLE_COLLECTSTATIC=False
 
